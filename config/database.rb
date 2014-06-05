27f498afd8c4f7542644e66e969bb7d807e5caf2
JuanManuelVallejos <juanmanuelvallejos2@gmail.com>
2014-06-05 11:47:38 -0300

    Update database.rb
    como estaba antes

--- config/database.rb
+++ config/database.rb
@@ -29,25 +29,15 @@
 #   <<: *defaults
 #   database: example_production
 #
-postgres = URI.parse(ENV['DATABASE_URL'] || '')
 
-DataMapper::Base.configurations[:travis] = {
-  :adapter  => 'postgresql',
-  :encoding => 'utf8',
-  :database => postgres.path[1..-1], 
-  :host     => postgres.host
-  :username => postgres.user,
-  :password => postgres.password,
-  :host     => postgres.host
-}
 
 DataMapper.logger = logger
 DataMapper::Property::String.length(255)
 
 case Padrino.env
   when :development then DataMapper.setup(:default, 'sqlite3://' + Padrino.root('db', 'makaya2_development.db'))
-  #when :travis      then DataMapper.setup(:default, 'sqlite3::memory:')
+  when :travis      then DataMapper.setup(:default, 'sqlite3::memory:')
   when :test        then DataMapper.setup(:default, 'sqlite3::memory:')
-  when :staging 	then DataMapper.setup(:default, ENV['DATABASE_URL'])
+  when :staging 	  then DataMapper.setup(:default, ENV['DATABASE_URL'])
   when :production  then DataMapper.setup(:default, ENV['DATABASE_URL'])
 end
