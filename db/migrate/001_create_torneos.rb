1669e67c3c62ec05d2b36eea8d7a2df243f7b8c7
juanmottesi <juannpm159@gmail.com>
2014-06-11 11:01:31 -0300

    Cambie los tipos de create_torneos cant_equipos para ver si es eso lo que falla en heroku

--- db/migrate/001_create_torneos.rb
+++ db/migrate/001_create_torneos.rb
@@ -3,7 +3,7 @@ migration 1, :create_torneos do
     create_table :torneos do
       column :id, Integer, :serial => true
       column :nombre, DataMapper::Property::String, :length => 255
-      column :cant_equipos, DataMapper::Property::Integer
+      column :cant_equipos, Integer, :key => false, :min => 0, :max => 1024
       end
   end
 
