66f6799262268d77e7c77481ea0b08bd36ff8678
“juanmanuelvallejos” <juanmanuelvallejos2@gmail.com>
2014-06-11 03:27:32 -0300

    cambiando database para conexion a base de datos postgree (yo, fer)

--- config/database.rb
+++ config/database.rb
@@ -35,21 +35,11 @@
 DataMapper.logger = logger
 DataMapper::Property::String.length(255)
 
-postgres = URI.parse(ENV['DATABASE_URL'] || '')
-
-DataMapper::Base.configurations[:production] = {
-  :adapter  => 'postgresql',
-  :encoding => 'utf8',
-  :database => postgres.path[1..-1], 
-  :username => postgres.user,
-  :password => postgres.password,
-  :host     => postgres.host
-}
 
 case Padrino.env
   when :development then DataMapper.setup(:default, 'sqlite3://' + Padrino.root('db', 'makaya2_development.db'))
   when :travis      then DataMapper.setup(:default, 'sqlite3::memory:')
   when :test        then DataMapper.setup(:default, 'sqlite3::memory:')
   when :staging 	then DataMapper.setup(:default, ENV['DATABASE_URL'])
-  #when :production  then DataMapper.setup(:default, ENV['DATABASE_URL'])
+  when :production  then DataMapper.setup(:default, ENV['DATABASE_URL'])
 end
