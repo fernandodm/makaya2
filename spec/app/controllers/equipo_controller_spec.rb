78164776623d9289c4f745ad744f130411e8e3c3
juanmottesi <juannpm159@gmail.com>
2014-06-10 23:38:58 -0300

    Se mejora los test de equipo y torneo

--- spec/app/controllers/equipo_controller_spec.rb
+++ spec/app/controllers/equipo_controller_spec.rb
@@ -2,7 +2,26 @@ require 'spec_helper'
 
 describe "EquipoController" do
      
-     
+     describe 'get :index' do
+
+	  it 'Quiero que responda ok y que muestre equipo/index' do
+	  	Makaya2::App.any_instance.should_receive(:render).with('equipo/list')
+	  	get '/equipo'
+	    last_response.should be_ok
+	  end
+	  
+    end
+
+    describe 'get :latest' do
+
+	  it 'Quiero que responda ok y que muestre equipo/latest' do
+	  	Makaya2::App.any_instance.should_receive(:render).with('equipo/list')
+	  	get '/equipo/latest'
+	    last_response.should be_ok
+	  end
+	  
+    end
+         
      describe 'get :new' do
 
 	  it 'Quiero que responda ok y que muestre equipo/new' do
@@ -11,5 +30,6 @@ describe "EquipoController" do
 	    last_response.should be_ok
 	  end
 	  
-    end
+    end 
+    
 end
