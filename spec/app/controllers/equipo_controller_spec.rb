1e2334466658290019e987c00489b94b0426b4d8
juanmottesi <juannpm159@gmail.com>
2014-06-17 21:00:18 -0300

    Se arreglo los test del controller de equipo y se borraron achivos con ~ al final

--- spec/app/controllers/equipo_controller_spec.rb
+++ spec/app/controllers/equipo_controller_spec.rb
@@ -2,36 +2,35 @@ require 'spec_helper'
 
 describe "EquipoController" do
               
-describe 'get :index' do
+  describe 'get :misEquipos, with => :torneo_id' do
 
-	  it 'Quiero que responda ok y que muestre equipo/index' do
+	  it 'Quiero que responda ok y que muestre equipo/list' do
 	  	Makaya2::App.any_instance.should_receive(:render).with('equipo/list')
-	  	get '/equipo'
+	  	get 'equipo/misEquipos/1'
 	    last_response.should be_ok
 	  end
 	  
     end
 
-    describe 'get :latest' do
+  describe 'get :editar_crear, with => :torneo_id' do
 
-	  it 'Quiero que responda ok y que muestre equipo/latest' do
-	  	Makaya2::App.any_instance.should_receive(:render).with('equipo/list')
-	  	get '/equipo/latest'
-	    last_response.should be_ok
-	  end
-	  
-    end
-         
-    describe 'get :new' do
+      it 'Quiero que responda ok y que muestre equipo/new' do
+        Makaya2::App.any_instance.should_receive(:render).with('equipo/new')
+        get 'equipo/editar_crear/2'
+        last_response.should be_ok
+      end
+        
+  end
+  
+  describe 'get :editar, with => :torneo_id' do
 
-	  it 'Quiero que responda ok y que muestre equipo/new' do
-	  	Makaya2::App.any_instance.should_receive(:render).with('equipo/new')
-	  	get '/equipo/new'
-	    last_response.should be_ok
-	  end
-	  
-	end
-       
+      it 'Quiero que responda ok y que muestre equipo/editar' do
+        Makaya2::App.any_instance.should_receive(:render).with('equipo/edit')
+        get 'equipo/editar/1'
+        last_response.should be_ok
+      end
+        
+  end
     
     
 end
