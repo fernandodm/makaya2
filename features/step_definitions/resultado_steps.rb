c66a91f77e17acd81465ad27af743c15184aaef9
“dimegliof” <dimegliofernando@gmail.com>
2014-06-25 02:58:56 -0300

    se agrego steps del resultado

---
+++ features/step_definitions/resultado_steps.rb
@@ -0,0 +1,48 @@
+Given(/^tiene un equipo llamado "(.*?)"$/) do |arg1|
+	visit '/torneo/latest'
+	click_link('Ver Equipos')
+	first(:link, 'Crear equipo').click
+	fill_in('equipo[nombre_equipo]', :with => arg1)
+	click_button('Crear')
+end
+
+Given(/^otro equipo llamado "(.*?)"$/) do |arg1|
+  visit '/torneo/latest'
+	click_link('Ver Equipos')
+	first(:link, 'Crear equipo').click
+	fill_in('equipo[nombre_equipo]', :with => arg1)
+	click_button('Crear')
+end
+
+When(/^voy a ver fixture$/) do
+  visit '/torneo/latest'
+	click_link('Ver Fixture')
+end
+
+When(/^voy a agregar resultado$/) do
+  visit '/torneo/latest'
+	click_link('Ver Fixture')
+	first(:link, 'Agregar resultado').click
+end
+
+When(/^agrego (\d+)$/) do |arg1|
+  fill_in('partido[resultado1]', :with => arg1)
+end
+
+When(/^(\d+)$/) do |arg1|
+  fill_in('partido[resultado2]', :with => arg1)
+end
+
+When(/^agrego "(.*?)"$/) do |arg1|
+  fill_in('partido[resultado1]', :with => arg1)
+end
+
+When(/^"(.*?)"$/) do |arg1|
+ fill_in('partido[resultado2]', :with => arg1)
+end
+
+When(/^confirmo el resultado$/) do
+  click_button('Cargar')
+end
+
+
